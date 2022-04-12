0caa30a1c9800f98ae91e93b66d94b2f88ee7b98
SevenPain <nguyenhaanhkiem@gmail.com>
2022-04-13 00:29:56 +0700

    JWT Authentication

--- src/routers/PrivateRouter.tsx
+++ src/routers/PrivateRouter.tsx
@@ -4,11 +4,6 @@ import { useSelector } from 'react-redux';
 import { Redirect, Route, useLocation } from 'react-router-dom';
 import { RootState } from '../redux/rootReducer';
 import { getInfo } from '../redux/slice/appSlice/userSlice';
-// import { doGetCurrentUser } from '../redux/slice';
-// import { EToken } from '../constants/login';
-// import { logout } from '../helpers/app';
-// import { readCookie } from '../helpers/login';
-// import { RootState } from '../redux/reducers/rootReducers';
 import { useAppDispatch } from '../redux/store';
 
 export const PrivateRouter: React.FC<IPrivateRouter> = ({
@@ -32,7 +27,7 @@ export const PrivateRouter: React.FC<IPrivateRouter> = ({
   const isAccount = useSelector((state: RootState) => state.userSlice.isAccount);
   const fecthInfo = async () => {
     const check = (await dispatch(getInfo({ jwt: localStorage.getItem('accessToken') }))).payload;
-    if (check === true || check === false || String(typeof check) === 'object') {
+    if (check === true || check === false || String(typeof check) === 'object' || check === undefined) {
       setIsFectch(true);
     }
   };
@@ -77,7 +72,7 @@ export const PrivateRouter: React.FC<IPrivateRouter> = ({
         <Component {...props} />
       </Layout>
     ) : (
-      <Redirect to="/" />
+      <Redirect to="/student/login" />
     );
   };
 
